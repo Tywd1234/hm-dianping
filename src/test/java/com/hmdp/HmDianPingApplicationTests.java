ee625b09f10c8cb2cb7e201f1a7c0f72f99928ed
hjx <1746005830@qq.com>
2023-07-04 12:59:51 +0800

    秒杀下单-无锁-超卖

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,28 @@
 package com.hmdp;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.entity.Voucher;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
-class HmDianPingApplicationTests {
+public class HmDianPingApplicationTests {
 
-
+    @Test
+    public void test() {
+        Voucher voucher = new Voucher()
+                .setShopId(1L)
+                .setTitle("100元代金券")
+                .setSubTitle("周一至周日均可使用")
+                .setRules("全场通用\n无需预约\n可无限叠加")
+                .setPayValue(8000L)
+                .setActualValue(10000L)
+                .setType(1)
+                .setStock(100)
+                .setBeginTime(LocalDateTime.now())
+                .setEndTime(LocalDateTime.now().plusDays(2));
+        System.out.println(JSONUtil.toJsonStr(voucher));
+    }
 }
